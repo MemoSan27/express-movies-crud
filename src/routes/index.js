ff9e01968766a3e8280bb3114016b62bba000127
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-06 23:23:15 -0700

    actors crud added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const genreRouter = require('./genre.router');
+const actorRouter = require('./actor.router');
 const router = express.Router();
 
 router.use(genreRouter);
+router.use(actorRouter);
 
 module.exports = router;
